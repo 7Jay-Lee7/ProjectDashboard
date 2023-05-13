6d7a62c5b15a7ccf391278688b278aa516607e44
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-05-13 23:10:21 +0100

    Create index.js

---
+++ assets/index.js
@@ -0,0 +1,67 @@
+import AiImagery from "./AI-Imagery.png";
+import AnimeStore from "./Anime-Store.png";
+import Bell from "./Bell.png";
+import ChatAi from "./Chat-AI.png";
+import Checked from "./Checked.png";
+import ChevronDown from "./ChevronDown.png";
+import Codecademy from "./Codecademy.png";
+import CodingJourney from "./progress/CodingJourney.png";
+import CreditCard from "./CreditCard.png";
+import Home from "./Home.png";
+import FinanceDashboard from "./FinanceDashboard.png";
+import freeCodeCamp from "./freeCodeCamp.png";
+import JSMastery from "./JSMastery.png";
+import Learning from "./Learning.png";
+import Leaving from "./logout/Leaving.gif";
+import Logout from "./Logout.png";
+import Me from "./Me.png";
+import MenuBar from "./Menu.png";
+import mernDashboard from "./MERN-Dashboard.png";
+import Messages from "./Messages.png";
+import MyLogo from "./logo.png";
+import OdinProject from "./OdinProject.png";
+import Pencil from "./Pencil.png";
+import Portfolio from "./Portfolio-Hero.png";
+import Progress from "./Progress.png";
+import Settings from "./Settings.png";
+import SignUp from "./Sign-Up-Form.png";
+import SocialMedia from "./SocialMedia.png";
+import threeDShirt from "./Jays-3D-Shirt-HomePage.png";
+import User from "./User.png";
+import W3school from "./W3Schools.png";
+import YouTube from "./YouTube.png";
+
+export {
+  AiImagery,
+  AnimeStore,
+  Bell,
+  ChatAi,
+  Checked,
+  ChevronDown,
+  Codecademy,
+  CodingJourney,
+  CreditCard,
+  Home,
+  FinanceDashboard,
+  freeCodeCamp,
+  JSMastery,
+  Learning,
+  Leaving,
+  Logout,
+  Me,
+  MenuBar,
+  mernDashboard,
+  Messages,
+  MyLogo,
+  OdinProject,
+  Pencil,
+  Portfolio,
+  Progress,
+  Settings,
+  SignUp,
+  SocialMedia,
+  threeDShirt,
+  User,
+  W3school,
+  YouTube,
+}
